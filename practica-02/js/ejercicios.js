55bc033e8ad8b6ad14b0fb4199d08924a4e6aeb8
eze.m.belmonte@gmail.com <eze.m.belmonte@gmail.com>
2025-04-25 19:48:55 -0300

    Prácticas

--- practica-02/js/ejercicios.js
+++ practica-02/js/ejercicios.js
@@ -161,3 +161,190 @@ console.log(arrayEquals(cc, dd));
 console.log(arrayEquals(ee, ee));
 
 console.log('\n');
+
+
+
+// ======================= EJERCICIO 06
+console.log('===================== EJERCICIO 06');
+
+function isInteger(num) {
+    return Number.isInteger(num);
+}
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 07
+console.log('===================== EJERCICIO 07');
+
+function isNumeric(num) {
+    // como isNaN devuelve true si NO es numero, lo niego para obtener true si recibo un num.
+    return !isNaN(num);
+}
+
+console.log(isNumeric("2a"));
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 08
+console.log('===================== EJERCICIO 08');
+
+function total(prices, amounts) {
+    let suma = 0;
+    // obtengo cada clave para poder acceder a su valor
+    for (let key in prices)
+        // verifico si existe esa clave en el vector de cantidad
+        if (amounts[key])
+            suma += prices[key] * amounts[key];
+    return suma;
+
+    /* NOTA: Si no me equivoco, lo ideal seria recorrer el objeto con menos elementos ya que
+    con hacer la verificacion del IF, me aseguro que no haya problema si no existe. */
+}
+
+var prices = {
+    milk: 48.90,
+    bread: 90.50,
+    butter: 130.13
+};
+
+var amounts = {
+    milk: 1,
+    bread: 0.5,
+    butter: 0.2
+};
+
+var amounts2 = {
+    bread: 1.5
+}
+
+//console.log(typeof prices);
+//console.log(prices, bread);
+//console.log(amounts["milk"]);
+console.log(total(prices, amounts));
+console.log(total(prices, amounts2));
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 09
+console.log('===================== EJERCICIO 09');
+
+var words = ['perro', 'gato', 'casa', 'árbol', 'nube', 'día', 'noche', 'zanahoria', 'babuino'];
+
+
+let temporal = {}
+// creo un objeto que de clave sea la palabra sin acento y el valor sea la palabra original
+for (let word of words) {
+    /* Tengo conocimiento de las expresiones regulares porque las he usado para verificar emails y contraseñas. 
+    Pero tuve que usar ChatGPT para saber cuál era la expresión regular para quitar acentos, ya que no la sabía.*/
+    let aux = word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    temporal[aux] = word;
+}
+
+// creo el array donde voy a guardar las palabras originales en orden
+var atoz = []
+
+// ordeno el objeto
+let temp = Object.fromEntries(Object.entries(temporal).sort());
+
+// recorro el obtejo ordenado y guardo el valor que es la palabra original
+for (let key in temp)
+    atoz.push(temporal[key]);
+
+// array ordenado
+console.log(atoz);
+
+// array ordenado inverso
+let ztoa = atoz.reverse();
+console.log(atoz);
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 10
+console.log('===================== EJERCICIO 10');
+
+// "copio" el arreglo original con el slice y ordenamos de acuerdo al length
+let orderlong = words.slice().sort((a, b) => a.length - b.length);
+console.log(orderlong);
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 11
+console.log('===================== EJERCICIO 1');
+
+function identity(x) {
+    return x;
+}
+
+var id = function(x) {
+    return x;
+};
+
+var iden = x => x;
+
+var identidad = identity;
+
+console.log(identity('Hola'));
+console.log(id('Hello'));
+console.log(iden('Buen día'));
+console.log(identidad('Buen día'));
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
+
+
+
+// ======================= EJERCICIO 1
+console.log('===================== EJERCICIO 1');
+
+console.log('\n');
